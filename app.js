a9fd6ec14fb1296d2ed79a530ffdd6bc7f47276b
Nguyen Phuc Vinh <nguyenphucvinh05072001@gmail.com>
2021-02-24 09:29:42 +0700

    update

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// 
+var cors = require('cors');
+var corsOption = {
+    origin: 'http://localhost:8000/'
+}
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -14,6 +20,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
+app.use(cors(corsOption));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
---
+++ config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "05072001",
+    DB: "",
+    dialect: "usermanagement",
+    pool: {
+        max: 10,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
